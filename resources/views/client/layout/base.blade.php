3e5b767d94fa1f29368ccc80dafa35bcf3636776
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2021-11-03 10:04:41 +0400

    Favicon

--- resources/views/client/layout/base.blade.php
+++ resources/views/client/layout/base.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href="/img/logo/1.png">
+
 
     @yield('head')
 
