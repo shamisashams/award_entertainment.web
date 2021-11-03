4eb35bff57ed61e6f6e4f015781b4592399ae8d2
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2021-11-03 09:35:55 +0400

    Contact us locale

--- resources/views/client/layout/partial/header.blade.php
+++ resources/views/client/layout/partial/header.blade.php
@@ -10,7 +10,7 @@
         <a href="{{ locale_route("client.gallery.index") }}" class="nav {{ request()->routeIs("client.gallery.index") || request()->routeIs('client.gallery.show') ? "active" : "" }}">@lang("client.gallery_navigation")</a>
         <a href="{{ locale_route('client.blog.index') }}" class="nav {{ request()->routeIs("client.blog.index") ? "active" : "" }}">@lang("client.blog_navigation")</a>
         <a href="{{ locale_route("about") }}" class="nav {{ request()->routeIs("about") ? "active" : "" }}">@lang("client.about_us_navigation")</a>
-        <a href="/#contact_section" class="nav">@lang("client.contact_us_navigation")</a>
+        <a href="{{locale_route("home.index")."/#contact_section"}}" class="nav">@lang("client.contact_us_navigation")</a>
     </div>
     <div id="nav_menu"></div>
 </div>
