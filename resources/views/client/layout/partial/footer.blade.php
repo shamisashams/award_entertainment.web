b085622a413cd93d0c18fad540cb252f01d0d2de
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2021-12-08 17:24:54 +0400

    Added button for company

--- resources/views/client/layout/partial/footer.blade.php
+++ resources/views/client/layout/partial/footer.blade.php
@@ -60,10 +60,13 @@
                     <img src="{{asset($company->mainFile->path.'/'.$company->mainFile->title)}}"
                          alt="{{$company->mainFile->title}}"/>
                 @endif
-                <div class="description">
-                    {!!  count($company->availableLanguage) >0 ? $company->availableLanguage[0]->description : "" !!}
-
-                </div>
+                    <div class="description">
+                        {!!  count($company->availableLanguage) >0 ? $company->availableLanguage[0]->description : "" !!}
+                        <button class='link'
+                                onclick="window.location.href='{{locale_route('client.company.show', $company->id)}}'"
+                            {{--                                href="{{locale_route('client.company.show', $company->id)}}"--}}
+                        >Visit Page</button>
+                    </div>
             </a>
         @endforeach
 
